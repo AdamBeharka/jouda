383cac5e4375b597ba08de671d4be68f4108c9ea
AdamBeharka <113507466+AdamBeharka@users.noreply.github.com>
2024-11-04 13:58:56 +0100

    Add files via upload

---
+++ tahacek.php
@@ -0,0 +1,21 @@
+<?php
+$cislo = 134;
+$des = 5.234;
+$zap = 235;
+$sting = "gypsy";
+//komentar
+
+if() {
+
+
+}
+else{
+
+}
+$pole = [1, 4, 5]
+var_dump($pole);
+echo("$pole");
+?>
+CTRL B schovat levou listu    
+parsing = zmena typu na typ
+
